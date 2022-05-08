098f53d69eb3a0fa3056e3148007a9ddda55fc17
PhilB <phil.bridge@ixapps.co.uk>
2022-05-08 08:55:35 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        // examples of expressions and statements, generally a semicolon completes a statement in Java
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("Thing"); // this method call is also a statment
+
+    }
+}
